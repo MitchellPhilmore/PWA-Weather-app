b8d17aeed6ba6f2342772d8df0b7a87ebda08213
unknown <mitchellphilmore@gmail.com>
2019-06-30 18:54:30 -0400

    initial update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import cloudyGif from './cloudy.gif'
+import clearGif from './tb.gif'
+import rainGif from './rainday2.gif'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let backgrounds = {
+  Clouds:cloudyGif,
+  Clear:clearGif,
+  Rain: rainGif
+
+}
+
+
+
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      weatherData:[],
+      backgroundType:''
+
+    }
+  }
+  componentWillMount(){
+    let apiKey = '73ec45f3936fe17133d8c9f13c187b62'
+
+    let key = `http://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&q=philadelphia&units=imperial&count=1`
+
+    axios.get(key)
+    .then(response=>response)
+    .then(response=>{
+      console.log(response)
+      let filteredList = response.data.list.filter((data)=>data.dt_txt.includes('18:00:00'))
+      console.log(filteredList)
+      this.setState({weatherData:filteredList,backgroundType:filteredList[0].weather[0].main})
+    })
+  }
+  generateDayOfTheWeek =(date)=>{
+    let generatedDay = new Date(date).getDay()
+    let days = 'Sunday,Monday,Tuesday,Wedsday,Thursday,Friday, Saturday'.split(",")
+    return days[generatedDay]
+  }
+ 
+  render(){
+    
+    console.log(this.state.backgroundType)
+    
+    return(
+      this.state.weatherData.length > 0 &&
+      <>
+      <div className="container">
+      <div style={{ background: `url(${backgrounds[this.state.backgroundType]}) no-repeat center center fixed`, 
+      backgroundSize: 'cover',height: '100%'}} class="card row">
+        <div style={{color:'white'}} class="card-content">
+           {this.state.weatherData.map(day=>{
+             let imgIcon = `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`
+             return(
+               <>
+              <h2>{Math.ceil(day.main.temp)}<sup>&#xb0;</sup></h2>
+              <h2>{day.weather[0].description}</h2>
+              <div className="weekly-forcast col s12">
+                <div className="days">
+              <h5 className="col l6">{this.generateDayOfTheWeek(day.dt_txt)}</h5>
+                 <div className="col l1"></div>
+                 <img  className="col s3" id="weatherIcon" src={imgIcon}/>
+                 <div className="col l3"></div>
+                 <h5  className="degree"><span style={{fontSize:'110%',fontWeight:700}}>{Math.ceil(day.main.temp)}</span><sup>&#xb0;</sup> <span style={{marginLeft:'5px',fontWeight:200}}>{Math.floor(day.main.temp_min)}<sup>&#xb0;</sup></span></h5>
+              </div>
+              </div>
+              <br/>
+              <br/>
+              <br/>
+              </>
+           )})}
+           
+           
+      </div>
+      </div>
+       
+        
+      </div>
+     </>
+  
+      
+    )
+  
+}
+
 }
 
 export default App;
