f4a94b517fa192f72d6842843fbe956842c6bf3b
unknown <mitchellphilmore@gmail.com>
2019-06-30 22:03:14 -0400

    added media query for iphone 6s,7,8

--- src/App.js
+++ src/App.js
@@ -74,8 +74,7 @@ class App extends Component{
               </div>
               </div>
               <br/>
-              <br/>
-              <br/>
+             
               </>
            )})}
            
