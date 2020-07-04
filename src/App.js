a0db68b96eb464ae7311094927816def91c0b4bc
Mitch <Mitch@tinas-MacBook-Air.local>
2020-07-04 14:49:16 -0400

    Refactor:Update project

--- src/App.js
+++ src/App.js
@@ -1,118 +1,102 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
-import cloudyGif from "./cloudy.gif";
-import clearGif from "./tb.gif";
-import rainGif from "./rainday2.gif";
+import cloudyGif from "./images/cloudy.gif";
+import clearGif from "./images/tb.gif";
+import rainGif from "./images/rainday2.gif";
 import moment from "moment";
 
 let backgrounds = {
   Clouds: cloudyGif,
   Clear: clearGif,
-  Rain: rainGif
+  Rain: rainGif,
 };
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      weatherData: [],
-      backgroundType: ""
-    };
-  }
-  componentWillMount() {
-    let apiKey = "73ec45f3936fe17133d8c9f13c187b62";
-
-    let key = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&q=philadelphia&units=imperial&count=1`;
+const App = (props) => {
+  const [weatherData, setWeatherData] = useState([]);
+  const [backgroundType, setBackgroundType] = useState("");
+  useEffect(() => {
+    const apiKey = "73ec45f3936fe17133d8c9f13c187b62";
+    const key = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&q=philadelphia&units=imperial&count=1`;
 
     axios
       .get(key)
-      .then(response => response)
-      .then(response => {
+      .then((response) => response)
+      .then((response) => {
         console.log(response);
-        let filteredList = response.data.list.filter(data =>
+        let filteredList = response.data.list.filter((data) =>
           data.dt_txt.includes("18:00:00")
         );
         console.log(filteredList);
-        this.setState({
-          weatherData: filteredList,
-          backgroundType: filteredList[0].weather[0].main
-        });
+        setWeatherData(filteredList);
+        setBackgroundType(filteredList[0].weather[0].main);
       });
-  }
-  generateDayOfTheWeek = date => {
+  }, []);
+
+  const generateDayOfTheWeek = (date) => {
     let dayOfTheWeek = moment(date).format("dddd");
     return dayOfTheWeek;
   };
-  render() {
-    console.log(this.state.backgroundType);
 
-    return (
-      this.state.weatherData.length > 0 && (
-        <>
-          <div className="container">
-            <div
-              style={{
-                background: `url(${
-                  backgrounds[this.state.backgroundType]
-                }) no-repeat center center fixed`,
-                backgroundSize: "cover",
-                height: "100%",
-                width: "100vw"
-              }}
-              class="card row"
-            >
-              <div style={{ color: "white" }} className="card-content">
-                {this.state.weatherData.map(day => {
-                  let imgIcon = `https://openweathermap.org/img/wn/${
-                    day.weather[0].icon
-                  }@2x.png`;
-                  return (
-                    <>
-                      {/* <h2>{Math.ceil(day.main.temp)}<sup>&#xb0;</sup></h2> */}
-                      <div className="weekly-forcast col s12">
-                        <div className="days">
-                          {/* <h4 className="col s6 l12 m12 day">{this.generateDayOfTheWeek(day.dt_txt) }</h4> */}
-                          <div>
-                            <h4 className="col s6 l12 m12 day white-text">
-                              {this.generateDayOfTheWeek(day.dt_txt)}
-                            </h4>
-                            <img
-                              className="col s3"
-                              id="weatherIcon"
-                              src={imgIcon}
-                            />
-                            <h2 className="description">
-                              {day.weather[0].description}
-                            </h2>
-                          </div>
-
-                          <div className="col l3" />
-                          <h5 className="degree">
-                            <span style={{ fontSize: "110%", fontWeight: 700 }}>
-                              {Math.ceil(day.main.temp)}
-                            </span>
-                            <sup>&#xb0;</sup>{" "}
-                            <span
-                              style={{ marginLeft: "5px", fontWeight: 200 }}
-                            >
-                              {Math.floor(day.main.temp_min)}
-                              <sup>&#xb0;</sup>
-                            </span>
-                          </h5>
+  return (
+     weatherData.length > 0 && (
+      <>
+        <div className="container">
+          <div style={{
+              background:`url(${backgrounds[backgroundType]}) no-repeat center `,
+              backgroundSize: "100vh",
+              height: "100%",
+            
+            }} 
+            
+            class="card row"
+          >
+            <div style={{ color: "white" }} className="card-content">
+              {weatherData.map((day) => {
+                let imgIcon = `https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
+                return (
+                  <>
+                    {/* <h2>{Math.ceil(day.main.temp)}<sup>&#xb0;</sup></h2> */}
+                    <div className="weekly-forcast col s12">
+                      <div className="days">
+                        {/* <h4 className="col s6 l12 m12 day">{this.generateDayOfTheWeek(day.dt_txt) }</h4> */}
+                        <div>
+                          <h4 className="col s6 l12 m12 day white-text">
+                            {generateDayOfTheWeek(day.dt_txt)}
+                          </h4>
+                          <img
+                            className="col s3"
+                            id="weatherIcon"
+                            src={imgIcon}
+                          />
+                          <h2 className="description">
+                            {day.weather[0].description}
+                          </h2>
                         </div>
+
+                        <div className="col l3" />
+                        <h5 className="degree">
+                          <span style={{ fontSize: "110%", fontWeight: 700 }}>
+                            {Math.ceil(day.main.temp)}
+                          </span>
+                          <sup>&#xb0;</sup>{" "}
+                          <span style={{ marginLeft: "5px", fontWeight: 200 }}>
+                            {Math.floor(day.main.temp_min)}
+                            <sup>&#xb0;</sup>
+                          </span>
+                        </h5>
                       </div>
-                      <br />
-                    </>
-                  );
-                })}
-              </div>
+                    </div>
+                    <br />
+                  </>
+                );
+              })}
             </div>
           </div>
-        </>
-      )
-    );
-  }
-}
+        </div>
+      </>
+    )
+  );
+};
 
 export default App;
